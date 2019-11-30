28d262d819366d10add2d0e04cbb8d0a62e317fe
dannyozh <danielongzhihao@gmail.com>
2019-11-30 16:27:56 +0800

    created cards for all podcasts. Able to click and redirects to podcast show page

--- app/javascript/components/podcastindex/podcastindex.jsx
+++ app/javascript/components/podcastindex/podcastindex.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save';
-import Box from '@material-ui/core/Box';
-import ReactDOMServer from 'react-dom/server';
+import Typography from '@material-ui/core/Typography';
+
+
+
 
 const csrfToken = document.querySelector('[name=csrf-token]').content
 axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
 
 class Podcastindex extends React.Component {
     constructor() {
@@ -33,44 +39,54 @@ class Podcastindex extends React.Component {
 
 
     render() {
+        const classes = {
+            maxWidth: 345,
+            height: 140
+        }
+
         if (this.state.podcasts === null) {
             return false
         } else {
             const allPodcasts = this.state.podcasts.map((podcast, index) => {
                 return (<div>
-                    <ul>
-                        <li key={podcast.id}></li>
-                        <li>{podcast.podcast_title}</li>
-                        <li><img src={podcast.podcast_photo} /></li>
-                        <br />
-                    </ul>
+                    <Card
+                        className="main-card"
+                    >
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="podcast image"
+                                image={podcast.podcast_photo}
+                                title="podcast image"
+                                height="140"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {podcast.podcast_title}
+                                </Typography>
+                                <Typography variant="subtitle1" color="textPrimary" component="p">
+                                    Episode {podcast.episode_no}: {podcast.episode_name}
+                                </Typography>
+                                <Typography variant="body1" color="textSecondary" component="p">
+                                    Excerpt: {podcast.description}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button href={'/podcasts/' + podcast.id} size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                    <br />
                 </div>);
             });
             return (
                 <div>
-                    <Box>
+                    <Container>
                         <h1>Hi from podcast INDEX.jsx</h1>
                         {allPodcasts}
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="small"
-                            className="button"
-                            id={this.state.podcastID}
-                            startIcon={<SaveIcon />}
-                        >
-                            Subscribe
-                        </Button>
-                        <Button onClick={() => { this.savePodcast() }}
-                            variant="contained"
-                            color="secondary"
-                            size="small"
-                            className="save-button"
-                            startIcon={<SaveIcon />}
-                        >
-                            Save
-                        </Button>
-                    </Box>
+                    </Container>
                 </div >
             );
         }
